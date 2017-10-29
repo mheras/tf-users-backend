7213e909df6ca97f0d121534d2298a479f65543c
Martin Heras <mheras@gmail.com>
2017-10-29 00:00:10 -0300

    Added UsersController.

--- src/main/java/com/tf/users/UsersController.java
+++ src/main/java/com/tf/users/UsersController.java
@@ -1,4 +1,13 @@
 package com.tf.users;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UsersController {
+
+    @RequestMapping("/")
+    public String sayHi() {
+        return "Hello there!";
+    }
 }
